a7be38ac49c0da76c457cdba39eefb63c2ab7ac1
av <Arthur.Varkentin@novatec-gmbh.de>
2020-09-16 17:17:44 +0200

    pass frame object from main (tested)

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, redirect, url_for, request, render_template, Response
 from werkzeug.utils import secure_filename
 from gevent.pywsgi import WSGIServer
 from webapp import VehicleDistance
+import cv2
 
  
 camera_id = {}
@@ -11,19 +12,33 @@ def main():
     
     return render_template("index.html")
 
-def gen(camera): 
-    while True:  
-        frame = camera.main()
-        if frame != "":
+def gen(camera):
+
+    make_video_file = 10 # Make video of first n frames
+
+    if make_video_file > 0:
+        fc = 0
+        out = cv2.VideoWriter('output.avi', cv2.VideoWriter_fourcc('M','J','P','G'), 10, (camera.width, camera.height))
+
+    while True: 
+        jpg_bytestream, frame = camera.main()
+        if jpg_bytestream != "":
             yield (b'--frame\r\n'
-                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+                b'Content-Type: image/jpeg\r\n\r\n' + jpg_bytestream + b'\r\n\r\n')
+        
+        if make_video_file > 0:
+            if fc <= make_video_file:
+                out.write(frame)
+                fc = fc + 1
+            elif fc == make_video_file + 1:
+                out.release()
 
 
 @app.route('/video_feed')
 def video_feed():
-    #id = 'rtsp://170.93.143.139/rtplive/470011e600ef003a004ee33696235daa'
+    id = 'rtsp://170.93.143.139/rtplive/470011e600ef003a004ee33696235daa'
     #id = 'https://www.youtube.com/watch?v=EbyOoMg191Y'
-    id  = 2    # Webcam with id 1
+    #id  = 2    # Webcam with id 2
     return Response(gen(VehicleDistance(id)), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/get_response', methods = ['POST']) 
